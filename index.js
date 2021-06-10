1cc5277a278e24a9b6b27d1067b6f28542a4eeeb
ponnarasi <85145307+ponnarasik@users.noreply.github.com>
2021-06-10 22:23:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let num = 1001;
+console.log(String(num).length);
+//Returns 'undefined'.
+//console.log(num.length);
+//let length= string(num.length);
+//console.log(length);
+
+//Use type conversion to print the length (number of digits) of an integer.
+//console.log(typeof length);
+
+
+
+//Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+
+//let num1=123.45;
+//console.log(typeof num1.length );
+
+
+//Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+
+if (String(num).includes ('.'))
+{
+console.log(String(num).length-1);
+}
+else{
+console.log(String(num).length);
+}
